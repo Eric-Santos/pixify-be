8908b611646422f718d46aedb540fe61cbd553b8
eric <eric355402@yahoo.com>
2020-07-19 19:22:44 -0600

    included cors in server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const PexelsRouter = require("./pexels/pexels-router.js");
 // const SpotifyRouter = require("./spotify/spotify-router.js");
 
 const server = express();
+server.use(cors());
 
 server.use(express.json());
 
